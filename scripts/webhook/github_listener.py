86a9457829ae6617e3ec3341b19b8d02f71297a2
fmondaca <f.mondaca@uni-koeln.de>
2018-11-20 15:57:44 +0100

    listener up

--- scripts/webhook/github_listener.py
+++ scripts/webhook/github_listener.py
@@ -83,7 +83,7 @@ def github_payload():
                     commits_url = commits_url.replace('{/sha}', '/' + sha)
                     logger.info('commits_url:   ' + commits_url)
                     req = requests.get(commits_url)
-                    commits_json = json.loads(req)
+                    commits_json = req.json()
                     files = commits_json['files']
                     re_indexed = []
                     for file in files:
